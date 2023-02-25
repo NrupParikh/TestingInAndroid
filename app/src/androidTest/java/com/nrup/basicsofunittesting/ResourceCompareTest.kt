a441a884aaad02d6e14cba7d0ea8d0757bea02d5
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2023-02-25 18:00:33 +0530

    - @Before and @After added in Test Case

--- app/src/androidTest/java/com/nrup/basicsofunittesting/ResourceCompareTest.kt
+++ app/src/androidTest/java/com/nrup/basicsofunittesting/ResourceCompareTest.kt
@@ -1,14 +1,29 @@
 package com.nrup.basicsofunittesting
 
 import android.content.Context
+import android.util.Log
 import androidx.test.core.app.ApplicationProvider
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 //https://www.youtube.com/watch?v=PsoLeJOh30o
 
 class ResourceCompareTest {
 
-    private var resourceCompare = ResourceCompare()
+    private lateinit var resourceCompare: ResourceCompare
+
+    // Execute the resource before our test case(s)
+    @Before
+    fun setup(){
+        resourceCompare = ResourceCompare()
+    }
+
+    // Execute after the test case(s). Generally to clean up the DB object.
+    @After
+    fun tearDown(){
+        Log.d("TAG","Test cases are completed. Clean up the object(s)")
+    }
 
     @Test
     fun stringResourceSameAsGivenString_returnsTrue() {
@@ -16,6 +31,7 @@ class ResourceCompareTest {
         val result = resourceCompare.isEqual(context, R.string.app_name, "BasicsOfUnitTesting")
         assert(result)
     }
+
 
     @Test
     fun stringResourceSameAsGivenString_returnsFalse() {
